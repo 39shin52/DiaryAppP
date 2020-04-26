8ea2cffac10097f949916a5d81f4893b84f44605
shin52 <b1018088@fun.ac.jp>
2020-04-26 16:34:02 +0900

    add Xcode Workspace

---
+++ DiaryAppP/Pods/Target Support Files/Pods-DiaryAppP-DiaryAppPUITests/Pods-DiaryAppP-DiaryAppPUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaryAppP_DiaryAppPUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DiaryAppP_DiaryAppPUITests
+@end
---
+++ DiaryAppP/Pods/Target Support Files/Pods-DiaryAppPTests/Pods-DiaryAppPTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaryAppPTests : NSObject
+@end
+@implementation PodsDummy_Pods_DiaryAppPTests
+@end
